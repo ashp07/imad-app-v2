c2b8e7a9147414c5c1dd0351243739ce9feec2cd
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-12 03:03:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,54 @@ var app = express();
 app.use(morgan('combined'));
 
 
+ var articleOne = {
+  title:'Article | one',
+  heading:'Article one',
+  date:'feb11 2017',
+  content: `       <p>
+                    The content of article one is today's date. The content of article one is today's date. The content of article one is today's date. The content of article one is today's date. The content of article one is today's date
+                  </p>
+                   
+                  <p>
+                       The content of article one is today's date. The content of article one is today's date. The content of article one is today's date. The content of article one is today's date. The content of article one is today's date
+                  </p>`
+};
+
+function createTemplate (data) 
+{
+    var title= data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+
+var htmlTemplate= `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+        <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+      <div class="container">
+          <div>
+             <a href='/'>Home</a>  
+          </div>
+            ${heading}     
+          <div>
+            ${date}
+          </div>
+        ${content}
+      </div>
+   </body>
+</html>
+   `;
+   return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
