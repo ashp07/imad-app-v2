21f1628840cc244c743ec91a873bf13bc234af46
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-12 17:44:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var element = document.getElementById('main-text');
 element.innerHTML=' Testing javascript' ;
 
 //try to drag the image in browser
-var image = document.getElementById('madi');
-image.onClick =function()
+var img = document.getElementById('madi');
+img.onClick =function()
  {
-     image.style.marginLeft = '100px';
+     img.style.marginLeft = '100px';
  };
