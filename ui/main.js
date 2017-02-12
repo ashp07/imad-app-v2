37514d7254f6fd6c5c13c99386448631cc9137ce
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-12 17:36:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //doing some changing
 var element = document.getElementById('main-text');
 element.innerHTML=' Testing javascript' ;
+
+//try to drag the image in browser
+var image = getElementById('img');
+image.onClick =function()
+ {
+     image.style.marginLeft='100px';
+ };
