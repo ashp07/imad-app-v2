b234e11ab28a14a55d3babfb09a4f50e59a4f963
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-13 12:59:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/profile.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
