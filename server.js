b78593cb13bd15a1162ec1f1647566bc3c807095
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-17 19:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,11 @@ var htmlTemplate= `
    return htmlTemplate;
 }
 
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -82,11 +87,7 @@ app.get('/:articleName', function (req,res) {
 });
 
 
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
