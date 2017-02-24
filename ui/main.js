688f2d1675351cc3a95aea39c1bc2005dd40df63
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-24 16:26:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,26 @@
 // counter
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick= function()
 {
+    //Create a request object
+    var request=new XMLHttpRequest();
     
+    //Capture the Response
+    request.onReadyStateChange=function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+           //Take some action
+           if(request.status == 200){
+              var counter= request.responseText;
+              var span= document.getElementById('count');
+              span.innerHTML = counter.toString();
+           }
+        } 
+        //Do nothing
+    };
     
+    //Making a request
+    request.open('GET','http://ashp07.imad.hasura-app.io/counter' , true);
+    request.send(null);
     
-    
-    //Rendering
-    counter= counter + 1;
-    var span= document.getElementById('count');
-    span.innerHTML = counter.toString();
 };
