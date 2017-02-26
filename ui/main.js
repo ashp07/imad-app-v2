664c1c832b8d6009147726ffcf6cf88ecd595067
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-26 15:29:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,13 @@ button.onclick = function()
     var request=new XMLHttpRequest();
     
     //Capture the Response
-    request.onReadyStateChange = function(){
-        if(request.readyState == XMLHttpRequest.DONE){
+    request.onReadyStateChange = function()
+    {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
            //Take some action
-           if(request.status == 200){
+           if(request.status == 200)
+           {
               var counter= request.responseText;
               var span= document.getElementById('count');
               span.innerHTML = counter;
@@ -25,18 +28,20 @@ button.onclick = function()
 };
 
   //Submit Name
-
   var submit = document.getElementById('submit_btn');
   submit.onclick = function()
   {
        //Create a request object
-    var request=new XMLHttpRequest();
+       var request=new XMLHttpRequest();
     
-    //Capture the Response
-    request.onReadyStateChange = function(){
-        if(request.readyState == XMLHttpRequest.DONE){
+       //Capture the Response
+       request.onReadyStateChange = function()
+       {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
            //Take some action
-           if(request.status == 200){
+           if(request.status == 200)
+           {
               //Capture a list of names and render it as a list
                    var names = request.responseText;
                    name= JSON.parse(names);
@@ -50,7 +55,7 @@ button.onclick = function()
            }
         } 
         //Do nothing
-  };
+       };
         
     //Make a request to the server and send the name
     var nameInput = document.getElementById('name');
