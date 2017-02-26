aea41a2f63d5274e0a9acb3a23a592da0154dff8
Ashish Kumar Pandey <ash07.pandey@gmail.com>
2017-02-26 14:47:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,39 +10,53 @@ button.onclick = function()
     request.onReadyStateChange = function(){
         if(request.readyState == XMLHttpRequest.DONE){
            //Take some action
-           if(request.status == 200){  //200 means successfull response
-              //Capture a list of names and render it as a list
-                 var names = request.responseText;
-                 names = JSON.parse(names);
-                 
-                  var list ='';
-                    for (var i=0; i<names.length; i++)
-                      {
-                     list += '<li>' + names[i] + '</li>';
-                      }
-                   var ul = document.getElementById('namelist');
-                   ul.innerHTML = list;
-      
+           if(request.status == 200){
+              var counter= request.responseText;
+              var span= document.getElementById('count');
+              span.innerHTML = counter;
            }
         } 
         //Do nothing
     };
     
     //Making a request
-    request.open('GET','http://ashp07.imad.hasura-app.io/aubmit-name?name=' +name , true);
+    request.open('GET','http://ashp07.imad.hasura-app.io/counter' , true);
     request.send(null);
-    
 };
 
   //Submit Name
-  var nameInput = document.getElementById('name');
-  var name =  nameInput.value;
+
   var submit = document.getElementById('submit_btn');
   submit.onclick = function()
   {
-    //Make a request to the server and send the name
+       //Create a request object
+    var request=new XMLHttpRequest();
     
-
+    //Capture the Response
+    request.onReadyStateChange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+           //Take some action
+           if(request.status == 200){
+              //Capture a list of names and render it as a list
+                   var names = ['name1','name2','name3','name4'];
+                   var list ='';
+                   for (var i=0; i<names.length; i++)
+                   {
+                   list += '<li>' + names[i] + '</li>';
+                   }
+                   var ul = document.getElementById('namelist');
+                   ul.innerHTML = list;
+           }
+        } 
+        //Do nothing
+  };
+        
+    //Make a request to the server and send the name
+    var nameInput = document.getElementById('name');
+    var name =  nameInput.value;
+    request.open('GET','http://ashp07.imad.hasura-app.io/submit-name?name=' +name , true);
+    request.send(null);
+      
   };
   
   
